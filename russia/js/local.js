00bb262fa65adae344c3d6982bcff626b9a40219
Zhang Sanfeng <123456@qq.com>
2018-01-01 23:25:32 +0800

    左移 右移

--- russia/js/local.js
+++ russia/js/local.js
@@ -4,6 +4,24 @@
 
 var Local = function () {
     var game; // 游戏对象
+
+    // 绑定键盘事件
+    var bindKeyEvent = function () {
+        document.onkeydown = function (e) {
+            if (e.keyCode == 38) {  //up
+
+            } else if (e.keyCode == 39) {  //right
+                game.right();
+            } else if (e.keyCode == 40) {  //down
+                game.down();
+            } else if (e.keyCode == 37) {  //left
+                game.left();
+            } else if (e.keyCode == 32) {  //space
+
+            }
+        }
+    }
+
     // 开始
     var start = function () {
         var doms = {
@@ -12,6 +30,7 @@ var Local = function () {
         };
         game = new Game();
         game.init(doms);
+        bindKeyEvent();
     };
     // 导师出API
     this.start = start;
--- russia/js/square.js
+++ russia/js/square.js
@@ -14,3 +14,42 @@ var Square = function () {
     // 原点
     this.origin = {x: 0, y: 0};
 };
+
+// 检测数据是否合法（下移）
+Square.prototype.canDown = function (isValid) {
+    var test = {};
+    test.x = this.origin.x + 1;
+    test.y = this.origin.y;
+    return isValid(test, this.data);
+};
+
+// 下移
+Square.prototype.down = function () {
+    this.origin.x = this.origin.x + 1;
+};
+
+// 检测数据是否合法（左移）
+Square.prototype.canLeft = function (isValid) {
+    var test = {};
+    test.x = this.origin.x;
+    test.y = this.origin.y - 1;
+    return isValid(test, this.data);
+};
+
+// 左移
+Square.prototype.left = function () {
+    this.origin.y = this.origin.y - 1;
+};
+
+// 检测数据是否合法（右移）
+Square.prototype.canRight = function (isValid) {
+    var test = {};
+    test.x = this.origin.x;
+    test.y = this.origin.y + 1;
+    return isValid(test, this.data);
+};
+
+// 右移
+Square.prototype.right = function () {
+    this.origin.y = this.origin.y + 1;
+};
