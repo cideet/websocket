bdccc63bce2e03c3d9cd41731db8be6c62c88ba7
张三丰 <123456@qq.com>
2018-01-16 22:25:24 +0800

    实现简单聊天功能

--- demo3/wsServer.js
+++ demo3/wsServer.js
@@ -13,22 +13,24 @@ var server = ws.createServer(function (conn) {
     console.log("New connection");
     clientCount++;
     conn.nickname = 'user' + clientCount;
-    broadcast(conn.nickname + ' coming in');
-    conn.on("text", function (str) {
+    broadcast(conn.nickname + ' coming in');  //有人进来的时候
+    conn.on("text", function (str) {  //有人发消息的时间
         console.log("Received " + str);
-        broadcast(str);
+        broadcast('user' + clientCount + ' say: ' + str);
     });
-    conn.on("close", function (code, reason) {
+    conn.on("close", function (code, reason) {  //有人离开的时候
         console.log("Connection closed");
         broadcast(conn.nickname + ' leave');
     });
-    conn.on('error', function (err) {
+    conn.on('error', function (err) {  //发生错误的时候
         console.log('handle error');
         console.log(err);
     });
 }).listen(PORT);
 
+// 给每个客户端发送消息
 function broadcast(str) {
+    // server.connections 所有连接
     server.connections.forEach(function (connection) {
         connection.sendText(str);
     })
