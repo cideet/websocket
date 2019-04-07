bccbad6010a3980e7c7d4d0b602218f89a7195b2
张三丰 <123456@qq.com>
2019-04-07 19:02:16 +0800

    websocket->(new .. onmessage .. send)

--- workerman/my_chat/vendor/gatewayworker/Applications/YourApp/start_gateway.php
+++ workerman/my_chat/vendor/gatewayworker/Applications/YourApp/start_gateway.php
@@ -21,7 +21,9 @@ use \Workerman\Autoloader;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 // gateway 进程，这里使用Text协议，可以用telnet测试
-$gateway = new Gateway("tcp://0.0.0.0:8282");
+//$gateway = new Gateway("tcp://0.0.0.0:8282");  //三丰改
+$gateway = new Gateway("Websocket://0.0.0.0:8282");
+
 // gateway名称，status方便查看
 $gateway->name = 'YourAppGateway';
 // gateway进程数
