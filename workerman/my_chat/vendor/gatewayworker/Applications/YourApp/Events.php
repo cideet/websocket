c748b8b6e560ee0d4f329762091c8ce627c985f8
张三丰 <123456@qq.com>
2019-04-07 22:06:22 +0800

    gatewayWorker实现简单的在线聊天

--- workerman/my_chat/vendor/gatewayworker/Applications/YourApp/Events.php
+++ workerman/my_chat/vendor/gatewayworker/Applications/YourApp/Events.php
@@ -31,35 +31,51 @@ class Events
     /**
      * 当客户端连接时触发
      * 如果业务不需此回调可以删除onConnect
-     * 
+     *
      * @param int $client_id 连接id
      */
     public static function onConnect($client_id)
     {
-        // 向当前client_id发送数据 
-        Gateway::sendToClient($client_id, "Hello $client_id\r\n");
-        // 向所有人发送
-        Gateway::sendToAll("$client_id login\r\n");
+//        // 向当前client_id发送数据
+//        Gateway::sendToClient($client_id, "Hello $client_id\r\n");
+//        // 向所有人发送
+//        Gateway::sendToAll("$client_id login\r\n");
+
+        global $num;
+        $data = ['type' => 'login', 'id' => $client_id, 'data' => 'Hello' . $client_id];
+        Gateway::sendToClient($client_id, json_encode($data));
+        $data2 = ['type' => 'login', 'id' => $client_id, 'data' => $client_id . '进入'];
+        Gateway::sendToAll(json_encode($data2));
     }
-    
-   /**
-    * 当客户端发来消息时触发
-    * @param int $client_id 连接id
-    * @param mixed $message 具体消息
-    */
-   public static function onMessage($client_id, $message)
-   {
-        // 向所有人发送 
-        Gateway::sendToAll("$client_id said $message\r\n");
-   }
-   
-   /**
-    * 当用户断开连接时触发
-    * @param int $client_id 连接id
-    */
-   public static function onClose($client_id)
-   {
-       // 向所有人发送 
-       GateWay::sendToAll("$client_id logout\r\n");
-   }
+
+    /**
+     * 当客户端发来消息时触发
+     * @param int $client_id 连接id
+     * @param mixed $message 具体消息
+     */
+    public static function onMessage($client_id, $message)
+    {
+        $mData = json_decode($message, true);
+        if (!$mData) {
+            return;
+        }
+        switch ($mData['type']) {
+            case 'say':
+                $data = ['type' => 'msg', 'id' => $client_id, 'data' => $mData['data']];
+                Gateway::sendToAll(json_encode($data));
+                break;
+        }
+    }
+
+    /**
+     * 当用户断开连接时触发
+     * @param int $client_id 连接id
+     */
+    public static function onClose($client_id)
+    {
+        // 向所有人发送
+        // GateWay::sendToAll("$client_id logout\r\n");
+        $data = ['type' => 'quit', 'id' => $client_id, 'data' => $client_id . 'quit'];
+        Gateway::sendToAll(json_encode($data));
+    }
 }
